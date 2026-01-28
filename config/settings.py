b9919e10b0c127503a920da949ebea982e531e7d
Alexandr <krapalok@mail.ru>
2026-01-28 17:14:34 +0300

    Initial commit: project structure, DeepSeek service and config

---
+++ config/settings.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+#грузим переменные из env файла
+load_dotenv()
+
+#Телеграм
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+#ДикПик:
+DEEPSEEK_API_KEY = os.getenv("TELEGRAM_TOKEN")
+DEEPSEEK_BASE_URL = "https://api.deepseek.com/v1"
+DEEPSEEK_MODEL = "deepseek-chat" #выбираем быструю модель, нам ее хватит 
+
+#serper.dev
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
